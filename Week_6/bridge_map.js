e5b688e8a995a3f3e611e8bf6ed6dd4b45719f6b
Ryan <li5823od@go.minnstate.edu>
2022-02-18 17:05:25 -0500

    2/18 lab update, part 2 complete

--- Week_6/bridge_map.js
+++ Week_6/bridge_map.js
@@ -1,7 +1,7 @@
 // Bridge map script with info in objects nested in an array
 
-let usCenterCoordinates = [44.96, -93.2]
-let zoomLevel = 4
+let usCenterCoordinates = [39.67, -96.54]
+let zoomLevel = 3.7
 
 let map = L.map('bridge-map').setView(usCenterCoordinates, zoomLevel)
 
@@ -14,36 +14,38 @@ let bridges = [
         name: "Verrazano-Narrows Bridge",
         citystate: "New York, NY",
         span: "1298.4",
-        location: "40.6066, -74.0447"
+        location: [40.6066, -74.0447]
     },
     {
         name: "Golden Gate Bridge",
         citystate: "San Francisco and Marin, CA",
         span: "1280.2",
-        location: "37.8199, -122.4783"
+        location: [37.8199, -122.4783]
     },
     {
         name: "Mackinac Bridge",
         citystate: "Mackinaw and St Ignace, MI",
-        span: "1158.0",
-        location: "45.8174, -84.7278"
+        span: "1158",
+        location: [45.8174, -84.7278]
     },
     {
         name: "George Washington Bridge",
         citystate: "New York, NY and New Jersey, NJ",
-        span: "1067.0",
-        location: "40.8517, -73.9527"
+        span: "1067",
+        location: [40.8517, -73.9527]
     },
     {
         name: "Tacoma Narrows Bridge",
         citystate: "Tacoma and Kitsap, WA",
         span: "853.44",
-        location: "47.2690, -122.5517"
+        location: [47.2690, -122.5517]
     }
 ]
 
 bridges.forEach( bridge => {
-    let marker = L.marker(bridge.location).addTo(map)
+    let marker = L.marker(bridge.location)
+        .bindPopup(`${bridge.name}` + '<br>' + `${bridge.citystate}` + '<br>' + `${bridge.span} meters`)
+        .addTo(map)
 })
 
 
