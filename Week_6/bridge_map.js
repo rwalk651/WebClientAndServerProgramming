2be87cb4a385d6f4d5f549314c350c3e151556a6
Ryan <li5823od@go.minnstate.edu>
2022-02-18 16:19:20 -0500

    2/18 lab update

--- Week_6/bridge_map.js
+++ Week_6/bridge_map.js
@@ -1,9 +1,25 @@
 let bridges = [
     {
-        // fill in bridge data as objects nested in array - do the same with map chart
-    }
-]
+        name: "Verrazano-Narrows Bridge",
+        citystate: "New York, NY",
+        span: "1298.4",
+        location: "40.6066, -74.0447"
+    },
+    {
+        name: "Golden Gate Bridge",
+        citystate: "San Francisco and Marin, CA",
+        span: "1280.2",
+        location: "37.8199, -122.4783"
+    },
+    {
+        name: "Mackinac Bridge",
+        citystate: "Mackinaw and St Ignace, MI"
+        span: "1158.0"
+        location: "45.8174, -84.7278"
+    },
 
+]
+// fill in bridge data as objects nested in array - do the same with map chart
 bridges.forEach( bridge => {
     let marker = L.marker(bridge.location).addTo(map)
 })
