e54c2d43c9123e1aa343a1ceaa99c56f0654e6f9
Ryan <li5823od@go.minnstate.edu>
2022-02-18 22:18:02 -0500

    2/18 lab update, part 2 complete

--- Week_6/bridge_map.js
+++ Week_6/bridge_map.js
@@ -14,31 +14,31 @@ let bridges = [
     {
         name: "Verrazano-Narrows Bridge",
         citystate: "New York, NY",
-        span: "1298.4",
+        span: [1298.4],
         location: [40.6066, -74.0447]
     },
     {
         name: "Golden Gate Bridge",
         citystate: "San Francisco and Marin, CA",
-        span: "1280.2",
+        span: [1280.2],
         location: [37.8199, -122.4783]
     },
     {
         name: "Mackinac Bridge",
         citystate: "Mackinaw and St Ignace, MI",
-        span: "1158",
+        span: [1158],
         location: [45.8174, -84.7278]
     },
     {
         name: "George Washington Bridge",
         citystate: "New York, NY and New Jersey, NJ",
-        span: "1067",
+        span: [1067],
         location: [40.8517, -73.9527]
     },
     {
         name: "Tacoma Narrows Bridge",
         citystate: "Tacoma and Kitsap, WA",
-        span: "853.44",
+        span: [853.44],
         location: [47.2690, -122.5517]
     }
 ]
@@ -46,7 +46,7 @@ let bridges = [
 bridges.forEach( bridge => {
     let marker = L.marker(bridge.location)
         .bindPopup(`${bridge.name}` + '<br>' + `${bridge.citystate}` + '<br>' + `${bridge.span} meters`)
-        .addTo(map)
+        .addTo(map);
 })
 
 let canvas = document.querySelector('#bridge-chart')
@@ -61,18 +61,43 @@ function getRandomColor() {     // https://stackoverflow.com/questions/1484506/r
     return color;
 }
 
-let bridgeName = bridges.map( bridge => bridge.name)
-let bridgeSpan = bridges.map( bridge => bridge.span)
+let bridgeName = function() {
+    let x = []
+    for ( let i = 0 ; i < bridges.length ; i++) {
+        x.push(bridges[i].name)
+    }
+    return x
+}
 
-mapChart = new Chart(ctx, {
+/*mapChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: [`${bridgeName}`],
+        labels: [`${bridgeName()}`],
         datasets: [{
             label: 'Span (meters)',
             data: [`${bridgeSpan}`],
-            backgroundColor: [`${getRandomColor()}`, `${getRandomColor()}`,
+            backgroundColor: ['red', 'blue', 'green', 'yellow', 'purple']/*[`${getRandomColor()}`, `${getRandomColor()}`,
                 `${getRandomColor()}`, `${getRandomColor()}`, `${getRandomColor()}`]
+        }]
+    }, options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
+*/
+chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [`${bridgeName()}`],
+        datasets: [{
+            label: 'Number of votes',
+            data: [`${bridgeSpan()}`],   // this is the chart data
+            backgroundColor: ['red', 'blue', 'yellow', 'green']
         }]
     }, options: {
         scales: {
