ba6f286480bc94b47a5879898cadc54e36209410
rwalk651 <li5823od@go.minnstate.edu>
2022-03-01 17:20:58 -0500

    3/1 update

--- Week_7/fetch.js
+++ Week_7/fetch.js
@@ -3,6 +3,13 @@ let url = 'https://api.wheretheiss.at/v1/satellites/25544'
 let issLat = document.querySelector('#iss_lat')
 let issLong = document.querySelector('#iss_long')
 
+let map = l.map('iss-map').setView([0, 0], 1);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
 fetch(url).then( (res) => {
     return res.json()   // response data is sent to issData
 }).then( (issData) => {
