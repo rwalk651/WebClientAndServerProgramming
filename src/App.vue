3b8bed3a05eb86d0664cfb7fa0b086a41c219eaa
rwalk651 <li5823od@go.minnstate.edu>
2022-04-21 01:55:24 -0400

    would you rather vu component app

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
 
     <h1>Would you rather?</h1>
 
-    <would-you-rather v-bind:question="wyrQuestion"
-    v-bind:answer1="wyrAnswer1"
-    v-bind:answer2="wyrAnswer2"
-
+    <would-you-rather
+      v-bind:question="wyrQuestion"
+      v-bind:answer1="wyrAnswer1"
+      v-bind:answer2="wyrAnswer2"
+      v-on:answer-changed="answerChanged"
 
     >
     </would-you-rather>
@@ -24,16 +25,26 @@ export default {
   },
   data() {
     return {
-      wyrQuestion: 'Would you rather be the author of a popular book or a musician in a band who released a popular album?'
+      wyrQuestion: 'Would you rather be the author of a popular book or a musician in a band who released a popular album?',
       wyrAnswer1: 'Author',
       wyrAnswer2: 'Musician',
       userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks you chose ${choice}`
     }
   }
 }
 </script>
 
 <style>
+
+body {
+  background-color: coral;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -41,5 +52,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: cornflowerblue;
 }
 </style>
--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="hello">
+  <div class="wyr">
 
     <h2>Please make your choice...</h2>
 
     <h3>{{ question }}</h3>
+
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
 
   </div>
 </template>
@@ -15,6 +22,16 @@ export default {
     question: String,
     answer1: String,
     answer2: String,
+  },
+  data() {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+      this.$emit('answer-changed', this.choice)
+    }
   }
 }
 </script>
@@ -24,15 +41,8 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.wyr {
+  background-color: aqua;
 }
 </style>
